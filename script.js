3894ebd2bb4c7f806bcfaf8609148e35347fbc04
strickergt128 <dilancastro7@hotmail.com>
2016-11-12 18:19:22 +0100

    host

---
+++ script.js
@@ -0,0 +1,11 @@
+$.getJSON ("paises.json", function (data) {
+    console.log(data)
+});
+// $.ajax({
+//   dataType: "json",
+//   // url: ,
+//   data: data,
+//   success:success,
+// }).then(function(data){
+//   console.log(data)
+// });
